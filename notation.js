ae7becbbdc09362fd98fa863d105f8a2eb738c83
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-07 19:09:50 +0600

    notation

---
+++ notation.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'Lal Nil HOlod mia',
+    profession: 'Trafic Surgeon',
+    age:30,
+    address: 'dhaka'
+}
+
+
+// dot notation
+const prof1 = person.profession
+console.log(prof1)
+
+// bracket notation
+const prof2 = person['profession']
+const Pname = 'profession'
+const prof3 = person[Pname]
+console.log(prof3)
+console.log(prof2)
